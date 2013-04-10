93a222698564680398140e119c918ca3f3241fdc
kuixiang <itxiangkui@gmail.com>
2013-04-10 18:07:49 +0800

    业务系统配置-测试

--- modules/hydra-example/src/test/java/com/jd/bdp/trigger/impl/MockTest.java
+++ modules/hydra-example/src/test/java/com/jd/bdp/trigger/impl/MockTest.java
@@ -16,9 +16,18 @@
 
 package com.jd.bdp.trigger.impl;
 
+import com.jd.bdp.hydra.agent.support.Configuration;
+import com.jd.bdp.hydra.dubbo.HydraConfiger;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
-
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Properties;
 /**
  * User: xiangkui
  * Date: 13-4-1
@@ -26,10 +35,57 @@ import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
  */
 public class MockTest extends AbstractDependencyInjectionSpringContextTests {
     private Trigger trigger;
+    private HydraConfiger configer;
     @Override
     protected String[] getConfigLocations() {
-        String[] location = {"/dubbo-service-context.xml"};
+        String[] location = {
+                "/dubbo-service-context.xml",//业务spring上下文
+                "/hydra-config.xml" //hydra标配spring上下文
+        };
         return location;
+    }
+
+    @Test
+    public void testConfiger() throws Exception {
+        //1：获取 Hydra感知的配置信息
+        Configuration envData=configer.getConfig();
+        String appEnvName=envData.getApplicationName();
+        List<String> serviceEnvList=envData.getServices();
+        //2：获取用户定义的配置的信息
+        //2.1 服务配置
+        List<String> serviceConfigList = new ArrayList<String>();
+        String packageName="com.jd.bdp.service.inter";
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        Enumeration<URL> resources = classLoader.getResources(packageName.replace('.', '/'));
+        List<File> dirs = new ArrayList<File>();
+        while (resources.hasMoreElements()) {
+            URL resource = resources.nextElement();
+            dirs.add(new File(resource.getFile()));
+        }
+        for (File file : dirs.get(0).listFiles()) {
+            if (file.getName().endsWith(".class"))
+                serviceConfigList.add(packageName + "." + file.getName().substring(0,file.getName().length() - 6));
+        }
+        //2.2 AppName配置
+        String appConfigName="";
+        try{
+            Properties prop = new Properties();
+            InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("dubbo.properties");
+            prop.load(in);
+            in.close();
+            appConfigName=prop.getProperty("dubbo.application.name").trim();
+        }   catch (Exception e){
+            e.printStackTrace();
+        }
+        //3 判断逻辑
+        for(String config:serviceConfigList){
+            Assert.assertTrue(serviceEnvList.contains(config));
+        }
+        for(String config:serviceEnvList){
+            Assert.assertTrue(serviceConfigList.contains(config));
+        }
+        Assert.assertEquals(appConfigName, appEnvName);
+        Thread.sleep(10);
     }
     @Test
     public void testTriggerService() throws Exception {
@@ -40,7 +96,16 @@ public class MockTest extends AbstractDependencyInjectionSpringContextTests {
     public Trigger getTrigger() {
         return trigger;
     }
+
     public void setTrigger(Trigger trigger) {
         this.trigger = trigger;
+    }
+
+    public HydraConfiger getConfiger() {
+        return configer;
+    }
+
+    public void setConfiger(HydraConfiger configer) {
+        this.configer = configer;
     }
 }

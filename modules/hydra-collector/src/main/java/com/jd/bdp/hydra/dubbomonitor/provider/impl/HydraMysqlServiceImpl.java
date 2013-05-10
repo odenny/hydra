df6bee77b42fb65d0442b8585d7ea47cc2992328
shell0dh <ryan66141@gmail.com>
2013-05-10 17:07:43 +0800

    updatemysql

--- modules/hydra-collector/src/main/java/com/jd/bdp/hydra/dubbomonitor/provider/impl/HydraMysqlServiceImpl.java
+++ modules/hydra-collector/src/main/java/com/jd/bdp/hydra/dubbomonitor/provider/impl/HydraMysqlServiceImpl.java
@@ -6,28 +6,54 @@ import com.jd.bdp.hydra.mysql.service.InsertService;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HydraMysqlServiceImpl implements HydraService {
     private InsertService insertService;
+    private ArrayBlockingQueue<List<Span>> queue = new ArrayBlockingQueue<List<Span>>(Integer.MAX_VALUE);
+    private ExecutorService executors = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     public InsertService getInsertService() {
         return insertService;
+    }
+
+    public HydraMysqlServiceImpl(){
+        for(int i = 0;i < 3 ;i++){
+            executors.execute(new InsertTask());
+        }
     }
 
     public void setInsertService(InsertService insertService) {
         this.insertService = insertService;
     }
 
+    class InsertTask implements Runnable{
+        @Override
+        public void run() {
+            while(true){
+                try {
+                    List<Span> span = queue.take();
+                    if(span != null){
+                        for(Span s : span){
+                            insertService.addSpan(s);
+                            insertService.addTrace(s);
+                            insertService.addAnnotation(s);
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    //ig
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
     @Override
     public boolean push(List<Span> span) throws IOException {
-        boolean rs = false;
-        if(span != null){
-            for(Span s : span){
-                insertService.addSpan(s);
-                insertService.addTrace(s);
-                insertService.addAnnotation(s);
-            }
-        }
-        return rs;
+        return queue.add(span);
     }
 }

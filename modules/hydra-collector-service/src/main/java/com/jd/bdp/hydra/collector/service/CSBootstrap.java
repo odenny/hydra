d90a6bae2b5f47ff719b63649271c37c62da9899
kuixiang <itxiangkui@gmail.com>
2013-05-22 18:14:34 +0800

    collector-service打包+架构文档-bd

--- modules/hydra-collector-service/src/main/java/com/jd/bdp/hydra/collector/service/CSBootstrap.java
+++ modules/hydra-collector-service/src/main/java/com/jd/bdp/hydra/collector/service/CSBootstrap.java
@@ -8,7 +8,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Date: 13-4-16
  * Time: 上午11:01
  */
-public class Bootstrap {
+public class CSBootstrap {
 
     public static void main(String[] strings){
         try {

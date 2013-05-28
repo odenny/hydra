f810ddfac6186dc1f7b5b14139c883756bd441e8
kuixiang <itxiangkui@gmail.com>
2013-05-28 09:53:46 +0800

    场景2测试环境搭建

---
+++ modules/hydra-test/hydra-test-integration/src/main/resources/scripts/linux/exp2/startD2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+MAINCLASSNAME="com.jd.bdp.hydra.benchmark.exp2.StartServiceD2"
+LOGNAME="$MAINCLASSNAME".log
+
+source ./env.sh
+TAIL_FILE="$LOG_DIR/$LOGNAME"
+java $SERVER_ARGS $MAINCLASSNAME > $TAIL_FILE &
